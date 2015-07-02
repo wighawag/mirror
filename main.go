2418449700aacaea3747f373903f9655c969ea61
Wighawag <wighawag@gmail.com>
2015-07-02 17:41:04 +0100

    WIP

--- main.go
+++ main.go
@@ -3,10 +3,20 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
 	//"github.com/kardianos/osext"
 	"gopkg.in/fsnotify.v1"
 )
+
+type ChangeEvent int
+
+const (
+	DELETE ChangeEvent = 0
+	WRITE  ChangeEvent = 1
+)
+
+func merge()
 
 func main() {
 	// folderPath, err := osext.ExecutableFolder()
@@ -14,28 +24,60 @@ func main() {
 	// 	log.Fatal(err)
 	// }
 
-	folderPath := "./"
+	command := "./"
 
-	command := ""
 	if len(os.Args) > 1 {
 		command = os.Args[1]
 	}
 
 	if command == "serve" {
-
+		serve()
 	} else if command != "" {
-		folderPath = command
+		mirror(command)
 	}
 
-	// port := os.Getenv("PORT")
-	// if port == "" {
-	// 	port = "9090"
-	// }
-	//
-	// httpErr := http.ListenAndServe(":"+port, nil)
-	// if httpErr != nil {
-	// 	log.Fatal("ListenAndServe: ", httpErr)
-	// }
+}
+
+func serve() {
+
+}
+
+// port := os.Getenv("PORT")
+// if port == "" {
+// 	port = "9090"
+// }
+//
+// httpErr := http.ListenAndServe(":"+port, nil)
+// if httpErr != nil {
+// 	log.Fatal("ListenAndServe: ", httpErr)
+// }
+
+func coalesce(in <-chan fsnotify.Event, out chan<- fsnotify.Event, period int) {
+	var event ChangeEvent
+	timer := time.NewTimer(0)
+
+	var timerCh <-chan time.Time
+	var outCh chan<- Event
+
+	for {
+		select {
+		case e := <-in:
+			event = mergeFSEvent(event, e)
+			if timerCh == nil {
+				timer.Reset(period * time.Millisecond)
+				timerCh = timer.C
+			}
+		case <-timerCh:
+			outCh = out
+			timerCh = nil
+		case outCh <- event:
+			event = NewEvent()
+			outCh = nil
+		}
+	}
+}
+
+func mirror(folderPath string) {
 
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
@@ -63,5 +105,5 @@ func main() {
 		log.Fatal(err)
 	}
 	<-done
-	log.Println("done")
+
 }
